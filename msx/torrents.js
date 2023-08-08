df97f4ae9e642edc1f53900a37ea264bca4deb92
damiva <96115589+damiva@users.noreply.github.com>
2023-08-08 05:58:15 +0000

    debug imdb

--- msx/torrents.js
+++ msx/torrents.js
@@ -183,7 +183,8 @@ function Torrents(){
         switch(d.message){
             case "imdb":
                 IMDB(d.data.imdb, function(i){
-                    if(i) TVXInteractionPlugin.executeAction("update:content:" + d.data.id, {image: i})
+                    if(i) TVXInteractionPlugin.executeAction("update:content:" + d.data.id, {image: i});
+                    console.log(i);
                 });
                 return true;
             case "drop":
