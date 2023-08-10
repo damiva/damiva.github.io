8d0573f49cd975523685836c9e52a8a8aaf21eaf
damiva <96115589+damiva@users.noreply.github.com>
2023-08-10 14:10:20 +0300

    Update ServeMSX.sh

--- ServeMSX.sh
+++ ServeMSX.sh
@@ -1,58 +1,61 @@
 #/bin/bash
-function ask(){
-    echo "$1? [Y/n]:" && read && [[ "$REPLY" -eq "n" ]] && return 1 || return 0
+function ask() {
+    echo -n "$1? [Y/n]: " && read && [[ "$REPLY" -eq "n" ]] && return 1 || return 0
 }
 
 EXE=ServeMSX
 DIR=/opt/$EXE
-URI=https://damiva.github.io/$EXE.linux
-case $(uname -m) in
-    armv7*) URI=$URI.arm;;
-    x86_64) URI=$URI.amd64;;
-    *) echo "Your architecture is not supported!" && exit 1;;
+URI=https://damiva.github.io/$EXE
+if [ ! -z "$1" ]; then DIR=$1; fi
+read SYS ARC <(uname -s -m)
+case $ARC in
+    armv7*) ARC=arm;;
+    x86_64) ARC=amd64;;
+    *) echo "Your architecture is not suppported" && exit 1;;
+esac
+case $SYS in
+    Linux) 
+        ARC=linux.$ARC
+        command -v systemctl >/dev/null || SYS=""
+        ;;
+    *) echo "Your OS is not supported!" && exit 1;;
 esac
 
 [ "$EUID" -ne 0 ] && echo "Please run me as root!" && exit 1
 
 [ -d $DIR ] || mkdir $DIR || exit
-wget -O $DIR/$EXE $URI || curl -o $DIR/$EXE $URI || exit
+curl -L -o $DIR/$EXE $URI/$ARC || exit
 
-if command -v systemctl >/dev/null && ask "Would you like to install $EXE as a service"
-then
-echo "Enter the address ([IP]:<PORT>) to listen to (leave blanc to use ':80'):" && read -r
-systemctl stop $EXE
-if ! cat << EOF > /etc/systemd/system/$EXE.service
-[Unit]
-Description=Media server for Media Station X
-Requires=network.target
-After=network-online.target syslog.target
-[Service]
-Type=simple
-WorkingDirectory=$DIR
-ExecStart=$DIR/$EXE -t $REPLY
-Restart=on-success
-[Install]
-WantedBy=multi-user.target
-EOF
-then
-    exit
-fi
-systemctl daemon-reload
-systemctl enable $EXE
-systemctl start $EXE
+if [ ! -z "$SYS" ] && ask "Would you like to install $EXE as a service"; then 
+    case $SYS in
+        Linux)
+            systemctl stop $EXE
+            echo -n "Address to listen to (lieave blanc to use ':80'): " && read -r PRT
+            get $DIR/$EXE.service $URI/$EXE.service || exit
+            while read line; do
+                case line in
+                    WorkingDirectory=*) echo "WorkingDirectory=$DIR";;
+                    ExecStart=*)        echo "ExecStart=$DIR/$EXE -t $PRT";;
+                    *)                  echo "$line";;
+                esac
+            done < (curl -L -s $URI/$EXE.service) > /etc/systemd/system/$EXE.service
+            systemctl daemon-reload
+            systemctl enable $EXE
+            systemctl start $EXE
+            ;;
+    esac
 fi
 
-if ask "Would you like to setup $EXE"
-then
-for md in video music photo
-do
-    echo "Enter absulute path to share $md files (leave blanc to skip):" && read -r
-    rm $DIR/$md
-    ln -s "$REPLY" $DIR/$md || echo "Error: $?"
-done
-echo "Enter the address (<IP>:<PORT>) of TorrServer (if it's not used or runs on this machine on port 8090, leave blanc):"
-read -r
-[[ -z "$REPLY" ]] && echo "http://$REPLY" > $DIR/torrserver
+if ask "Would you like to setup $EXE"; then
+    for md in video music photo
+    do
+        echo -n "Enter absulute path to share $md files (leave blanc to skip): "
+        read -r
+        [ -z "$REPLY" ] || ln -s "$REPLY" $DIR/$md
+    done
+    echo -n "Enter the address (<IP>:<PORT>) of TorrServer (if it's not used or runs on this machine on port 8090, leave blanc): "
+    read -r
+    [ -z "$REPLY" ] || echo "http://$REPLY" > $DIR/torrserver
 fi
 
 echo Done!
