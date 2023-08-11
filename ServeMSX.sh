e4e0621d72f8602009b79d07df36e0289e0d25e5
damiva <96115589+damiva@users.noreply.github.com>
2023-08-11 10:45:23 +0000

    init added & no autostart

--- ServeMSX.sh
+++ ServeMSX.sh
@@ -1,70 +1,57 @@
 #!/bin/bash
-function ask() {
-    if [[ "${1: -1}" == "?" ]]; then
-        read -p "$1 [Y/n]: " -n 1 -r && echo && [[ $REPLY =~ ^[Nn]$ ]] && return 1 || return 0
-    else
-        read -r -p "$1: " && [ -z "$REPLY" ] && return 1 || return 0
-    fi
-}
-function dnl() {
-    curl -L -o "$1" "$2" || wget -O "$1" "$2" || exit
-}
 [ $EUID -ne 0 ] && echo "Please run me as root!" && exit 1
 EXE=ServeMSX
-DIR=/opt/$EXE
-[ -z "$1" ] || DIR=$1
+case $1 in
+    )   DIR=/opt/$EXE;;
+    .)  DIR=$PWD;;
+    *)  DIR=$1;;
+esac
 URI=https://damiva.github.io/$EXE
-case $(arch) in
-    armv7*) ARC=arm;;
-    x86_64) ARC=amd64;;
-    *) echo "Your arch is not supported!" && exit 1;;
+read -r SYS ARC < <(uname -s -m)
+case $ARC in
+    aarch64*)   ARC=arm64;;
+    armv8*)     ARC=arm64;;
+    arm*)       ARC=arm;;
+    x86_64)     ARC=amd64;;
+    *) echo "Your arch '$ARC' is not suppported at this time!" && exit 1;;
 esac
-case $(uname) in
-    Linux)
-        ARC=linux.$ARC
-        command -v systemctl >/dev/null && SYS="systemd"
-        ;;
-    *)
-        echo "Your OS is not supported!" && exit 1
-        ;;
+case $SYS in
+    Linux)      SYS=linux;;
+    Darwin)     SYS=darwin;;
+    *) echo "Your OS '$SYS' is not supported at this time!" && exit 1;;
 esac
 
-echo -n "Loading $URI/$ARC to $DIR/$EXE..."
 [ -d $DIR ] || mkdir $DIR || exit
-dnl $DIR/$EXE $URI/$ARC
-chmod +x $DIR/$EXE || exit
-echo "done"
+curl -L -o $DIR/$EXE $URI/$SYS.$ARC && chmod +x $DIR/$EXE || exit
 
-if [ -n "$SYS" ] && ask "Would you like to install $EXE as a service?"
-then 
-    case $SYS in
-        systemd)
-            ask "Address to listen to (leave blanc to use ':80')"
-            echo -n "Installing and disbling the service..."
-            systemctl -q --now disable $EXE
-            dnl $DIR/$EXE.service $URI/$EXE.service
-            while read line; do
-                case $line in
-                    WorkingDirectory=*) echo "WorkingDirectory=$DIR";;
-                    ExecStart=*) echo "ExecStart=$DIR/$EXE -t $REPLY";;
-                    *) echo "$line"
-                esac
-            done < $DIR/$EXE.service > /etc/systemd/system/$EXE.service
-            rm $DIR/$EXE.service
-            systemctl daemon-reload
-            systemctl --now enable $EXE
-            echo "done"
-            ;;
-    esac
-fi
-
-if ask "Would you like to setup $EXE?"
+if read -p "Would you like to configure $EXE? [Y/n]: " -n 1 -r && [[ ! $REPLY =~ ^[Nn]$ ]]
 then
     for md in video music photo
     do
-        ask "Enter absulute path to share $md files (leave blanc to skip)" && ln -s "$REPLY" $DIR/$md
+        read -p "Enter absulute path to share $md files (leave blanc to skip): " -r && [ -n "$REPLY" ] && ln -s "$REPLY" $DIR/$md
     done
-    ask "Enter the address (<IP>:<PORT>) of TorrServer (if it's not used or runs on this machine on port 8090, leave blanc)" && echo "http://$REPLY" > $DIR/torrserver
+    read -p "Enter the address (<IP>:<PORT>) of TorrServer (if it's not used or runs on this machine on port 8090, leave blanc): " && [ -n "$REPLY" ] && echo "http://$REPLY" > $DIR/torrserver
+fi
+
+if read -p "Would you like to install $EXE as a service? [Y/n]: " -n 1 -r && [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+    read -r -p "Address to listen to (leave blanc to use default http): " PRT
+    if [[ "$SYS" == "linux" ]]; then SYS=$(ps --no-headers -o comm 1); fi
+    case $SYS in
+        systemd)
+            systemctl -q --now disable $EXE
+            while read line; do
+                case $line in
+                    WorkingDirectory=*) echo "WorkingDirectory=$DIR";;
+                    ExecStart=*) echo "ExecStart=$DIR/$EXE -t $PRT";;
+                    *) echo "$line"
+                esac
+            done < <(curl -L -s $URI/$EXE.service) > /etc/systemd/system/$EXE.service
+            systemctl daemon-reload
+            systemctl --now enable $EXE
+            ;;
+        *) echo "Your service system '$SYS' is not supported at this time!" && exit 1;;
+    esac
 fi
 
 echo Done!
