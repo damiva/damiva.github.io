5d3e034565170345ec9a10b33bf8d4995f6e2910
damiva <96115589+damiva@users.noreply.github.com>
2024-05-27 18:00:44 +0300

    Update ts.js

--- msx/ts.js
+++ msx/ts.js
@@ -38,8 +38,8 @@ function init(b){
         info.dictionary = window.location.origin + "/msx/russian.json";
         main.menu[i].data.items[0].label = "Switch to english";
     }
-    TVXInteractionPlugin.requestData("info:extended", function(d){
-        main.menu[i].data.underlay.items[2].headline = d.data.info.application.name + " " + d.data.info.application.version;
+    TVXInteractionPlugin.requestData("info", function(d){
+        main.menu[i].data.underlay.items[2].headline = d.info.application.name + " " + d.info.application.version;
         TVXServices.ajax.get("/msx/start.json", {
             success: function(d){main.menu[i].data.underlay.items[1].headline = d.name + " " + d.version},
             complete: function(){b.stop()}
