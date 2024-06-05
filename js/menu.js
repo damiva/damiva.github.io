12fc8d68ff4365a9ae01f9b274f708a073c51705
damiva <96115589+damiva@users.noreply.github.com>
2024-06-05 13:17:23 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,59 +1,151 @@
-TVXPluginTools.onReady(function() {
-    TVXInteractionPlugin.setupHandler(new function(){
-        var R = TVXService.storage.getBool("ts:russian", false),
-        S = new search(R), P = new playlist(),
-        M = {menu: [
-            {icon: "bookmarks", label: "{dic:trns|My torrents}", data: "request:interaction:trns@" + window.location.href},
-            {icon: "search", label: "{dic:srch|Search torrents}", data: "request:interaction:search@" + window.location.href},
-            {icon: "folder", label: "{dic:fls|My files", data: "request:interaction:/files/@" + window.location.href}
-        ], options: opts(
-            {key: "green", icon: "translate", label: R ? "Switch to english" : "Перевести на русский", data: {action: "russian"}},
-            {key: "yellow", label: "{dic:refresh|Refresh} {dic:caption:menu|menu}", action: "[cleanup|reload:menu]"}
-        )};
-        var L = function(d){return {
-            type: "list", reuse: false, cache: false, restore: false, extension: "{ico:msx-white:bookmarks} " + d.length,
-            template: {layout: "0,0,6,2", imageWidth: 1.3, imageFiller: "height"}, items: d.map(function(t){return {
-                id: t.hash,
-                headline: t.title,
-                image: t.poster,
-                icon: t.poster ? "" : "msx-white-soft:bookmark",
-                group: "{ico:" + (t.category == "movie" ? "movie" : t.category == "tv" ? "live-tv" : t.category == "music" ? "audiotrack" : "more-horiz") + "}",
-                titleFooter: "{ico:msx-white:attach-file} " + (t.torrent_size ? size(t.torrent_size) : "?"),
-                stamp: t.stat < 5 ? ("{ico:north} " + (t.active_peers || 0) + " / " + (t.total_peers || 0) + " {ico:south} " + (t.connected_seeders || 0)) : "",
-                stampColor: "msx-" + (t.stat == 4 ? "red" : t.stat == 3 ? "green" : "yellow"),
-                options: opts(
-                    {key: "red", label: "{dic:rem|Remove the torrent}", data: {action: "rem", hash: t.hash}},
-                    t.stat < 5 ? {key: "green", label: "{dic:drop|Drop the torrent}", data: {action: "drop", hash: t.hash}} : null,
-                    {key: "yellow", label: "{dic:refresh|Refresh} {dic:list|the list}", action: "[cleanup|reload:content]"}
-                ),
-                action: "content:request:interaction:" + t.hash + "@" + window.location.href
-            }})
-        }};
-        this.init = P.init();
-        this.ready = function(){M.logo = addr + "/logo.png"};
-        this.handleData = function(d){if(!S.handleData(d)) P.handleData(d)};
-        this.handleRequest = function(i, d, f){
-            switch(i){
-                case "init:":
-                    ajax("/files", function(d){
-                        M.menu[2].display = d ? true : false;
-                        f(M);
-                    }, function(){
-                        M.menu[2].display = false;
-                        f(M);
-                    });
-                    break;
-                case "trns":
-                    ajax("/torrents", {action: "list"}, function(d){
-                        f(L(d));
-                    }, function(e){
-                        TVXInteractionPlugin.error(e);
-                        f();
-                    });
-                    break;
-                default: if(!S.handleRequest(i, d, f)) P.handleRequest(i, d, f);
+var addr = window.location.protocol + "//";
+var menu = [
+    {icon: "bookmarks", label: "{dic:trns|My torrents}",    data: "request:interaction:trns@" + window.location.href},
+    {icon: "search",    label: "{dic:srch|Search torrents}",data: "request:interaction:keys@" + window.location.href},
+    {icon: "folder",    label: "{dic:files|My files}",      data: "request:interaction:access:{SERVER}@http://nb.msx.benzac.de/interaction"}
+]
+var trns = {
+    l: function(l){},
+    f: function(l, c){},
+    h: function(l, s){},
+    c: function(f, e, d){
+        var t = this;
+        if(d) TVXServices.ajax.get(addr + "/search/?query=" + encodeURIComponent(d.find), {
+            success: function(l){
+                if((l = t.f(l, d.cat)).length) f(t.h(l, d.find));
+                else f({action: "warn:{dic:empty|Nothing found}"});
+            },
+            error: e
+        });
+        else TVXServices.ajax.post(addr + "/torrents", '{"action":"list"}', {
+            success: function(l){f(t.h(t.l(l)))},
+            error: e
+        })
+    }
+}
+var trnt = {
+    d: {},
+    c: function(f, e, d, i){
+        var t = this;
+        if(d){
+            t.d = d;
+            f({action: "content:request:interaction:" + i});
+        } else TVXServices.ajax.get(
+            addr + "/stream/?stat&" + ["link", "title", "poster", "category"].map(function(k){return k + "=" + encodeURIComponent(t.d[k])}).join("&"),
+            {success: function(d){
+
+            }, error: e}
+        );
+    }
+}
+function keys(i, r){
+    var ks = [false, true].map(function(K){
+        var k = [{label: "1", key: "1", offset: K ? "1,0,0,0" : undefined}], l = K ? 1 : 0,
+            e = [
+                ["q","w","e","r","t","y","u","i","o","p","bracket_open","bracket_close"],
+                ["a","s","d","f","g","h","j","k","l","semicolon","quote"],
+                ["z","x","c","v","b","n","m","comma","period"]
+            ],
+            r = [
+                ["й","ц","у","к","е","н","г","ш","щ","з","х","ъ"],
+                ["ф","ы","в","а","п","р","о","л","д","ж","э"],
+                ["я","ч","с","м","и","т","ь","б","ю"]
+            ];
+        if(K) k.push({type: "space"});
+        for(var i = 2; i < 10; i++) k.push({label: i = TVXTools.strValue(i), key: i});
+        if(K) k.push({type: "space"});
+        k.push({label: "0", key: "0", offset: K ? "-1,0,0,0" : undefined});
+        for(var y = 0; y < 3; y++){
+            var b = K ? r : e, s = 10 + l * 2 - y * 1 - (y < 2 ? 1 : 2);
+            for(var x = 0; x < s + 1; x++){
+                var o = y == 1 ? .5 : y == 2 && x == 0 ? 1 : 0;
+                if(y == 1 && x == s || y == 2 && x == 1) k.push({type: "space"});
+                k.push({label: b[y][x], key: e[y][x], offset: (x == s ? "-" : "") + o + ",0,0,0"});
             }
         }
+        k.push(
+            {type: "space"},
+            {label: K ? "ё" : "'", key: K ? "accent" : "quote", offset: "-1,0,0,0"},
+            {label: "{ico:backspace}", titleFooter: "{ico:fast-rewind}", key: "delete|red", data: "bs", progress: 1, progressColor: "msx-red", offset: l + ",0,1,0"},
+            {type: "space"}
+        );
+        if(K) k.push({type: "space"});
+        k.push(
+            {label: "{ico:clear}", titleFooter: "{ico:skip-previous}", key: "home", data: "cl", offset: "0,0,1,0"}, {type: "space"},
+            {label: "{ico:space-bar}", titleFooter: "{ico:fast-forward}", key: "insert|yellow", data: " ", progress: 1, progressColor: "msx-yellow", offset: "0,0,1,0"}, {type: "space"},
+            {label: "{ico:language}", titleFooter: "{ico:skip-next}", key: "tab|end", data: "ck", offset: "0,0,1,0", id: "lang"}, {type: "space"}, {type: "space"}
+        );
+        if(K) k.push({type: "space"});
+        k.push({label: "{ico:done}", key: "green", data: "ok", progress: 1, progressColor: "msx-green", offset: (-l-1) + ",0,1,0"});
+        return k;
     });
+    this.d = function(d){
+
+    };
+    this.c = function(f){
+        
+    }
+}
+function init(f, e){TVXServices.ajax.post(addr + "/settings", '{"action":"get"}', {
+    success: function(d){
+        M.menu[1].enable = d.EnableRutorSearch;
+        TVXServices.ajax.get(A + "/files", {
+            success: function(d){M.menu[2].display = d ? true : false},
+            error: function(){M.menu[2].display = false},
+            complete: function(){f({logo: A + "/logo.png", menu: menu})}
+        });
+    },
+    error: e
+})}
+function acts(f, d){TVXServices.ajax.post(addr + "/torrents", JSON.stringify(d), {
+    success: function(){f({action: "[cleanup|reload:content|success]"})},
+    error: function(e){f({action: "error:" + e})},
+    dataType: "text"
+})}
+function opts(){
+    var k = ["red", "green", "yellow"];
+    var o = {headline: "{dic:caption:options|Opts}:", items: [], template: {
+        enumerate: false, layout: "0,0,8,1", type: "control", action: "execute:request:interaction:act"
+    }};
+    o.caption = o.headline;
+    for(var i = 0; i < arguments.length && i < k.length; i++) if(arguments[i]){
+        arguments[i].key = k[i];
+        arguments[i].icon = "msx-" + k[i] + ":stop";
+        o.items.push(arguments[i]);
+        o.caption += "{tb}{ico:" + arguments[i].icon + "} " + arguments[i].label
+    }
+    return o.items.length ? o.items : null;
+}
+function size(s){
+    var i = s == 0 ? 0 : Math.floor(Math.log(s) / Math.log(1024));
+    return (s / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i];
+};
+
+TVXPluginTools.onReady(function() {
+    TVXInteractionPlugin.setupHandler(new function(){
+        var b = new TVXBusyService(), k = {};
+        this.ready = function(){
+            var h = TVXServices.urlParams.get("host");
+            addr += h;
+            menu[2].data.replace("{SERVER}", h);
+            b.start();
+            TVXInteractionPlugin.requestData("info:dictionary", function(d){
+                k = new keys("trns", d.data.info.dictionary.name == "Русский");
+                b.stop();
+            })
+        };
+        this.handleData = function(d){b.onReady(function(){k.d(d.data)})};
+        this.handleRequest = function(i, d, f){b.onReady(function(){
+            var e = function(m){TVXInteractionPlugin.error(m); f()};
+            switch(i){
+                case "init": init(f, e); break;
+                case "keys": k.c(f); break;
+                case "trns": trns.c(f, e, d && d.data); break;
+                case "trn": trnt.c(f, e, d && d.data, i); break;
+                case "act": acts(f, d && d.data); break;
+                default: e("wrong request id: " + i);
+            }
+        })}
+    })
     TVXInteractionPlugin.init();
-});
+})
