f4b4449eb92b8cdee823175a5a7e1e5992898244
damiva <96115589+damiva@users.noreply.github.com>
2024-06-10 18:35:15 +0300

    Add files via upload

--- js/utils.js
+++ js/utils.js
@@ -12,11 +12,12 @@ function size(s){
     var i = s == 0 ? 0 : Math.floor(Math.log(s) / Math.log(1024));
     return (s / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i];
 }
-function opts(o, h, v){
+function opts(o, h, v, e){
     h = h || "{dic:caption:options|opt/menu}";
     var r = {caption: h + (v || ""), headline: h + ":", items: [], template: {
         layout: "0,0,8,1", type: "control", enumerate: false, imagePreload: true, action: "interaction:load:" + window.location.href
     }};
+    if(e) r.extension = e;
     o.forEach(function(o){if(o){
         if(o.key){
             o.progress = 1;
@@ -39,9 +40,3 @@ function prms(k, c){
     if(c) TVXServices.storage.set(k, v = !v);
     return v;
 }
-function proxy(u, d){
-    var g = d.data === undefined, s = {success: d.success, error: d.error}, t = d.dataType ? {dataType: d.dataType} : null;
-    u = addr + "/msx/proxy?url=" + encodeURIComponent(u);
-    if(d.header) u = u + "&" + Object.keys(d.header).map(function(k){return "header=" + encodeURIComponent(k + ":" + d.header[k])}).join("&");
-    TVXServices.ajax[g ? "get" : "post"](u, g ? s : d.data, g ? t : s, g ? undefined : t);
-};
