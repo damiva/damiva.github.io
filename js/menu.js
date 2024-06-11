5a74c0fb1c313b0c31fe6800f361bad752f2c1be
damiva <96115589+damiva@users.noreply.github.com>
2024-06-11 04:22:53 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -21,7 +21,7 @@ TVXPluginTools.onReady(function() {
                 type: "list", reuse: false, cache: false, restore: false,
                 extension: e ? "{ico:msx-yellow:warning}" : ("{ico:msx-white:bookmarks} " + d.length),
                 header: {items: [
-                    {type: "space", layout: "0,0,12,1", color: "msx-glass", imageWidth: 1, icon: "filter-alt"}
+                    {type: "space", layout: "0,0,12,1", color: "msx-glass", imageWidth: 1, headline: "{ico:filter-alt}{tb}{fic:filter|Filter}:"}
                 ].concat(["", "movie", "live-tv", "audiotrack", "more-horiz"].map(function(g, i){return {
                     type: "control", layout: ((i + 1) * 2) + ",0,2,1", icon: g || undefined, label: g ? "" : "{dic:label:no|No}",
                     extensionIcon: icon(G == g), data: g, action: "execute:request:interaction:trns@" + window.location.href
