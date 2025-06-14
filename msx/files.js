fc33675d21396494b075626eba71614eb8963709
damiva <96115589+damiva@users.noreply.github.com>
2025-06-14 13:41:05 +0300

    Update files.js

--- msx/files.js
+++ msx/files.js
@@ -1,1 +1,15 @@
-
+function files(addr){
+  this.handleRequest = function(i, _, f){
+    if(i[i.length - 1] != "/") return false;
+    TVXServices.ajax.get(addr + i, {
+      success: function(d){
+        d = 
+      },
+      error: function(e){
+        TVXInteractionPlugin.error(e);
+        f();
+      }
+    }, {dataType: "html"});
+    return true;
+  };
+}
